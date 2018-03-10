b8802020c5b2b60cf253662e2148bb2c21854416
Robert Mugabe Ongom <ongomr@Roberts-MacBook-Pro-i7.local>
2018-03-10 12:32:37 +0200

    Initail Upload of TripleO Local Lab Setup

---
+++ scripts/setup_locale.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo echo "LANG=en_US.utf-8" >> /etc/environment
+sudo echo "LC_ALL=en_US.utf-8" >> /etc/environment
---
+++ scripts/setup_uc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    uc_hostname=$1
+else
+  uc_hostname=ucsrv.lab
+fi
+
+sudo useradd stack
+sudo passwd stack  # specify a password
+echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack
+sudo chmod 0440 /etc/sudoers.d/stack
+su - stack
+sudo hostnamectl set-hostname $uc_hostname
+sudo hostnamectl set-hostname --transient $uc_hostname
+
+#   127.0.0.1   myhost.mydomain myhost
+#sudo yum install -y https://trunk.rdoproject.org/centos7/current-tripleo/python2-tripleo-repos-0.0.1-0.20171116021457.15e17a8.el7.centos.noarch.rpm
+sudo yum install -y https://trunk.rdoproject.org/centos7/current/python2-tripleo-repos-0.0.1-0.20180306114928.3fb8f7b.el7.centos.noarch.rpm
+sudo -E tripleo-repos -b newton current
+sudo -E tripleo-repos -b newton current ceph
+sudo yum install -y python-tripleoclient
+sudo yum install -y ceph-ansible
+sudo cp /mnt/configs/undercloud.conf  ~/undercloud.conf
+sudo chown stack:stack ~/undercloud.conf
+sudo -u stack openstack undercloud install
